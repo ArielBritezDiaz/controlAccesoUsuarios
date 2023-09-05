073a2e57b593be6c98e60b2fcda61f2925d2c876
Ariel Diaz <arielbritezdiaz@gmail.com>
2023-09-04 23:22:42 -0300

    Finalización de programa

--- registrar.php
+++ registrar.php
@@ -1,28 +1,70 @@
 <?php
 include("conexion.php");
 
-if(isset($_POST['registrar'])) {
+$usuarioForm = $_POST['usuario'];
+$contrasenia = $_POST['contrasenia'];
 
-    $usuarioForm = $_POST['usuario'];
-    $contrasenia = $_POST['contrasenia'];
+$contrasenia = password_hash($contrasenia, PASSWORD_DEFAULT);
 
-    $contrasenia = password_hash($contrasenia, PASSWORD_DEFAULT);
+function redimensionarImg($image, $medidaFinal) {
+    list($anchoOriginal, $alturaOriginal, $nroTipo) = getimagesize($image);
 
-    if(is_uploaded_file($_FILES['imagen']['tmp_name'])) {
-        $directoryFoto = $_FILES['imagen']['tmp_name'];
-        $nameFoto = $_FILES['imagen']['name'];
-
-        move_uploaded_file($dirFoto, $nameFoto);
-
-        $imagen = $nameFoto;
-
-        unlink($nameFoto);
-    } else {
-        echo "No se subió una imagen";
+    switch($nroTipo) {
+        case 2: 
+            $imageOriginal = imagecreatefromjpeg($image);
+            break;
+        case 3:
+            $imageOriginal = imagecreatefrompng($image);
+            break;
     }
 
-    $sqlInsert = "INSERT INTO usuarios(Nbr_u, Img_u, Pass_u) VALUES ('$usuarioForm', '$contrasenia', '$imagen')";
-    $sqlInsertar = mysqli_query($conexion, $sqlInsert); ? print("Inserción de nuevo usuario realizada con éxito") : print("Error al insertar nuevo usuario");
+    if($anchoOriginal > $alturaOriginal){
+        $anchoFinal = $medidaFinal;
+        $aspecto = $anchoOriginal / $alturaOriginal;
+        $altoFinal = $anchoFinal / $aspecto;
+
+    } else{
+        $altoFinal = $medidaFinal;
+        $aspecto = $anchoOriginal / $alturaOriginal;
+        $anchoFinal = $altoFinal * $aspecto;
+    }
+
+    $nuevaImagen = imagecreateTrueColor($anchoFinal, $altoFinal);
+    
+    imagecopyresampled($nuevaImagen, $imageOriginal, 0, 0, 0, 0, $anchoFinal, $altoFinal, $anchoOriginal, $alturaOriginal);
+    
+    imagedestroy($imageOriginal);
+
+    $calidad = 100;
+
+    $nombreImagen = time().'_'.$image;
+
+    imagejpeg($nuevaImagen, "imagenes/$nombreImagen", $calidad);
+
+    return $nombreImagen;
 }
+
+if(is_uploaded_file($_FILES['imagen']['tmp_name'])) {
+    $dirFoto = $_FILES['imagen']['tmp_name'];   
+    $nameFoto = $_FILES['imagen']['name'];
+
+    move_uploaded_file($dirFoto, $nameFoto);
+
+    $imagen = redimensionarImg($nameFoto, 100, 100);
+    unlink($nameFoto);
+} else {
+    $imagen = "./si.png";
+}
+
+$sql = "INSERT INTO usuarios(Nbr_u, Img_u, Pass_u) VALUES ('$usuarioForm', '$imagen', '$contrasenia')";
+$consultaInsertar = mysqli_query($conexion, $sql);
+
+echo "
+    <div>
+        <p>Nombre: '$usuarioForm'</p>
+        <p>Contraseña: '$contrasenia'</p>
+        <img src='imagenes/$imagen'></img>
+    </div>"
+;
 
 ?>
