d0ea36698272965482598b3fea0b7f02668244cd
MarianoJSB <mariano.n.aguiar@gmail.com>
2023-10-17 20:21:45 -0300

    Login styled

--- login.php
+++ login.php
@@ -8,19 +8,44 @@ include("conexion.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="iniciar.css">
+
+    <!-- Title font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Sen&display=swap" rel="stylesheet">
+
+    <!-- Input font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+
+    <!-- Font-Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
+
     <title>Login</title>
-    <div class="login">
-    <img src="Logo.svg" class="logo">
-    <p class="title">Iniciar sesion</p>
-    <form action=" " method="post" class="formu">
-        <input type="text" placeholder="Usuario" required name="usuario">
-        <input type="password" placeholder="Contraseña" required name="contrasenia" >
-        <input type="submit" value="Iniciar sesion" class="enviar" name="envio">
-    </form>
-        <a href="recuperar.php?rec">Olvide mi contraseña</a>
-    </div>
 </head>
 <body>
+    <div class="container">    
+        <div class="leftImage">
+        </div>
+        <div class="login">
+        <img src="logor&b.svg" class="logo">
+        <p class="title">Iniciar sesión</p>
+        <form action=" " method="post" class="formu">
+            <div class="input-container">
+                <input type="text" placeholder="Usuario" required name="usuario" pattern=".{8,}">
+                <i class="fa-solid fa-user" style="color: #cc0000;"></i>
+            </div>
+            <div class="input-container">
+                <input type="password" placeholder="Contraseña" required name="contrasenia" pattern=".{8,}">
+                <i class="fa-solid fa-lock" style="color: #cc0000;"></i>
+            </div>
+            <a href="recuperar.php?rec" class="recuperar">Olvide mi contraseña</a>
+            <input type="submit" value="Iniciar sesión" class="enviar" name="envio">
+        </form>
+            <a href="form_registro.html" class="registrar">¿No tienes cuenta? Registrate</a>
+        </div>
+    </div>
 </body>
 </html>
 <?php
