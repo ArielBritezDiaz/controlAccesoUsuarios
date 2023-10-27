65f12a5d678c259a8f576756872749bba3f85e5c
MarianoJSB <mariano.n.aguiar@gmail.com>
2023-10-27 20:26:56 -0300

    back arrow

--- recuperar.php
+++ recuperar.php
@@ -28,6 +28,7 @@ include("conexion.php");
     <?php
     if(isset($_GET['rec'])){
         ?>
+        <a href="login.php"><img src="src/images/backArrow.png"></a>
         <form action="" method="POST">
         <label for="">Recuperar contraseña</label>
         <div class="inputs">
