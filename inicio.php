766f1475151abfea2d42d9f36df9d1f43e54a7f5
Aaron8585 <avila.aaron@tecnica7.edu.ar>
2023-10-27 17:06:36 -0300

    footer

--- inicio.php
+++ inicio.php
@@ -13,8 +13,16 @@ if(isset($_SESSION['usuario'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/inicio.css">
     <title>Inicio</title>
 </head>
 <body>
+    <footer class="footer">
+            <img src="src/images/logo/logoraw.svg" class="logo">
+            <div class="info">
+                <p>© 2023 CMF engines</p>
+                <p>cmfengines@gmail.com</p>
+            </div>
+    </footer>
 </body>
 </html>
