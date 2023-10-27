b3fb43905830eef9a71aecbe87b9b35ea8658085
Aaron8585 <avila.aaron@tecnica7.edu.ar>
2023-10-27 19:09:14 -0300

    home finished

--- inicio.php
+++ inicio.php
@@ -3,8 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
-=======
 
     <!-- Font-Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
@@ -14,10 +12,13 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Open+Sans&display=swap" rel="stylesheet">
 
->>>>>>> b924b63704fa6fda8f46aaac9ce272285088eec2
     <link rel="stylesheet" href="styles/inicio.css">
     <title>Inicio</title>
-    <!-- Header y nav -->
+</head>
+<body>
+    <!-- Categories -->
+    <main>
+        <!-- Header y nav -->
     <header class="header">
         <nav class="nav">
             <img src="src/images/logo/logor&w.svg" class="logo">
@@ -43,8 +44,6 @@
         </nav>
         <div class="slider"></div>
     </header>
-    <!-- Categories -->
-    <main>
         <section class="categories" id="categories">
             <article class="cars">
                 <p>Autos</p>
@@ -59,15 +58,13 @@
                 <a href="#"><img src="src/images/categories/formula.png"></a>
             </article>
         </section>
-    </main>
-</head>
-<body>
-    <footer class="footer">
-            <img src="src/images/logo/logoraw.svg" class="logo">
+        <footer class="footer">
+            <img src="src/images/logo/logor&w.svg" class="logo2">
             <div class="info">
                 <p>© 2023 CMF engines</p>
                 <p>cmfengines@gmail.com</p>
             </div>
-    </footer>
+        </footer>
+    </main>
 </body>
 </html>
