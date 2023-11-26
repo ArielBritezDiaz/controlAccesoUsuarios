ee84198d294d2c8b0bc5274bd6e4737c6f874b39
MarianoJSB <mariano.n.aguiar@gmail.com>
2023-11-26 19:16:40 -0300

    DB updated : foreign key in articles table

--- control_acceso.sql
+++ control_acceso.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-11-2023 a las 23:05:05
+-- Tiempo de generación: 26-11-2023 a las 23:15:16
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -34,19 +34,42 @@ CREATE TABLE `articulos` (
   `precio` decimal(12,2) NOT NULL,
   `stock` int(6) NOT NULL,
   `imagen` varchar(200) NOT NULL,
-  `categoria` varchar(15) DEFAULT NULL
+  `categoria` varchar(15) DEFAULT NULL,
+  `color` varchar(10) DEFAULT NULL,
+  `estado` varchar(8) DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `articulos`
 --
 
-INSERT INTO `articulos` (`id_articulo`, `nombre`, `descripcion`, `precio`, `stock`, `imagen`, `categoria`) VALUES
-(1, 'hot wheels', '5 autos de hot wheels en caja nueva', 1200.00, 2, '1699313553_artcicle.jpg', 'Auto'),
-(2, 'Hot wheels big collection', 'Amazing hot wheels collection with 60 cars ', 78000.00, 1, '1699313671_artcicle.jpg', 'Auto'),
-(4, 'Formula 1 malboro', '--------', 234567.00, 3, '1699314118_artcicle.jpg', 'Formula'),
-(5, 'Formula 1 black', 'Unique f1 car black', 5600.00, 5, '1699314153_artcicle.jpg', 'Formula'),
-(8, 'Ferrari racing car scale', ' there are only 4 items in the whole world', 3245.00, 12, '1699314350_artcicle.jpg', 'Formula');
+INSERT INTO `articulos` (`id_articulo`, `nombre`, `descripcion`, `precio`, `stock`, `imagen`, `categoria`, `color`, `estado`, `id_usuario`) VALUES
+(1, 'hot wheels', '5 autos de hot wheels en caja nueva', 1200.00, 2, '1699313553_artcicle.jpg', 'Auto', NULL, NULL, NULL),
+(2, 'Hot wheels big collection', 'Amazing hot wheels collection with 60 cars ', 78000.00, 1, '1699313671_artcicle.jpg', 'Auto', NULL, NULL, NULL),
+(4, 'Formula 1 malboro', '--------', 234567.00, 3, '1699314118_artcicle.jpg', 'Formula', NULL, NULL, NULL),
+(5, 'Formula 1 black', 'Unique f1 car black', 5600.00, 5, '1699314153_artcicle.jpg', 'Formula', NULL, NULL, NULL),
+(8, 'Ferrari racing car scale', ' there are only 4 items in the whole world', 3245.00, 7, '1699314350_artcicle.jpg', 'Formula', NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedidos`
+--
+
+CREATE TABLE `pedidos` (
+  `id_pedido` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `articulos` varchar(20) NOT NULL,
+  `id_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `pedidos`
+--
+
+INSERT INTO `pedidos` (`id_pedido`, `fecha`, `articulos`, `id_usuario`) VALUES
+(1, '2023-11-26', ' 8/3245.00/5', 24);
 
 -- --------------------------------------------------------
 
@@ -59,44 +82,22 @@ CREATE TABLE `usuarios` (
   `Nbr_u` varchar(30) NOT NULL,
   `Pass_u` varchar(120) NOT NULL,
   `email_u` varchar(30) NOT NULL,
-  `token_u` int(11) NOT NULL
+  `token_u` int(11) NOT NULL,
+  `Foto_u` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`ID_u`, `Nbr_u`, `Pass_u`, `email_u`, `token_u`) VALUES
-(1, '', '$2y$10$z9pxJRwCB86//iX.UzQ6v.eONJhPXpGSTiuXfdqFskcH2gkDWSZ9a', '', 1),
-(2, 'mari', '12345', 'mariano.n.aguiar@gmail.com', 1),
-(3, 'mari', '444444', 'mariano.n.aguiar@gmail.com', 1),
-(4, 'Ariel', '909090', 'mariano.n.aguiar@gmail.com', 1),
-(5, 'aaro', '6767', 'mariano.n.aguiar@gmail.com', 1),
-(6, 'xd', 'xdxdx', 'aguiar.mariano@tecnica7.edu.ar', 1),
-(7, 'alan', '1311313', 'aguiar.mariano@tecnica7.edu.ar', 1),
-(8, 'alan', '3112321311', 'aguiar.mariano@tecnica7.edu.ar', 1),
-(9, 'axel3', '12331313', 'aguiar.mariano@tecnica7.edu.ar', 1),
-(10, 'lcui', '456', 'aguiar.mariano@tecnica7.edu.ar', 1),
-(11, 'lcui', '5555', 'aguiar.mariano@tecnica7.edu.ar', 1),
-(12, 'agu', '3332331', 'aguiar.mariano@tecnica7.edu.ar', 1),
-(13, 'agu', '444144221', 'mariano.n.aguiar@gmail.com', 1),
-(14, 'aguasdasdd', '634344554', 'mariano.n.aguiar@gmail.com', 1),
-(15, 'dsaadsa', '$2y$10$OpLXSj/MrCjDRPwNFBVtV.ZTvw2t.7/B3QxdoFtqxXmse0Q5090pi', 'mariano.n.aguiar@gmail.com', 1),
-(16, 'dsaadsa', '$2y$10$5eU4rMIBE6j6o2eJqvBBoufl9V7nbnkiyIOIkXxJ4l7DwszquFhji', 'mariano.n.aguiar@gmail.com', 1),
-(17, 'mariano', '$2y$10$AYoYTnf5l1lWQ8Z.Dkvbe.BtixpV7Vr0gnUEfDTRXC1L4WadxCnYG', 'mariano.n.aguiar@gmail.com', 1),
-(18, 'meri', '$2y$10$gaVAme/BeZQbPSng9pgETuvUSwnurUUL4l50Uga3.08vFjL2IEZZ6', 'mariano.n.aguiar@gmail.com', 1),
-(19, 'javier', '$2y$10$F8dgOrG/5mMnQmd3yNe7cebad1pzBxSQ0C9zr9KzjeBBFYj2Q9JjO', 'mariano.n.aguiar@gmail.com', 1),
-(20, '123', '$2y$10$70Yh2EKQsGJLx.WRQbSxW.J.R8GOkllENRvMw3wl7zbly.ro7Ty3.', 'mariano.n.aguiar@gmail.com', 1),
-(21, 'uiui', '$2y$10$DJSD8h6iIbi1koXd8goqfeOCuOV6Wzy4FOhKYakz1RYBSYokuR6lG', 'mariano.n.aguiar@gmail.com', 1),
-(22, 'uiui', '$2y$10$B6hxHYCbFTyb2a83W.VkruBR.hNh/w64FiqoVypJlvkcnQWDbL30e', 'mariano.n.aguiar@gmail.com', 1),
-(23, 'dassdasddadadas', '$2y$10$lN83.ldwtz4vK8oKFtpUze4PdoexXpHMMkn79AJp6vjwASKBqzm4y', 'migatten897@gmail.com', 1),
-(24, 'mariasdaddsad', '$2y$10$/Hmbs9tPU0h1.20o7ipZceWbkV7n.eyng9pxkUpq/ab3ITbP7oQeS', 'migatten879@gmail.com', 1),
-(25, 'mariasdaddsad', '$2y$10$V8DYD6pNGug6Zj4x9ZaEqe.04JuKbdnA1kYdHyaS28YjBQTLuDv1q', 'aaron.avila@tecnica7.edu.ar', 1),
-(27, 'mariano7', '$2y$10$HvkMJs8/qjovz2axkZb38.Ewm8Og.sGQwsYBJhYxIGM/V6yIYAMhC', 'rosita.melanito@gmail.com', 1),
-(28, 'mariano7', '$2y$10$LOA1b4eMHorKSkpLTuJKjODTNQFiDhQtiiCW8d9DeWbM85OUytqMi', 'rosita.melanito36@gmail.com', 1),
-(29, 'alanzelada12', '$2y$10$iYSjEsafX6RDwNlRhI1cjOKanF4s0lkXPq/EHZ5sJqEbBaShMi9qS', 'alanzelada9@gmail.com', 1),
-(30, 'daasdadaddsad', '$2y$10$vzjB1k71WA9Rpx7OTbE3COesWSW.Kstkuw7nkPfxwxOtwkEMsyVQG', 'adasdasdd@dadasdasd.com', 1),
-(38, 'siiiiiiiiiiiii', '$2y$10$nh12PAHqXGP2nC6Bjc1rKOWU2lEz83DPa9CTVWhkDZ.ZCaotY5Xj2', 'cmftresd@gmail.com', 1);
+INSERT INTO `usuarios` (`ID_u`, `Nbr_u`, `Pass_u`, `email_u`, `token_u`, `Foto_u`) VALUES
+(24, 'lionel messi', '$2y$10$/Hmbs9tPU0h1.20o7ipZceWbkV7n.eyng9pxkUpq/ab3ITbP7oQeS', 'migatten879@gmail.com', 1, ''),
+(25, 'mariasdaddsad', '$2y$10$V8DYD6pNGug6Zj4x9ZaEqe.04JuKbdnA1kYdHyaS28YjBQTLuDv1q', 'aaron.avila@tecnica7.edu.ar', 1, ''),
+(27, 'mariano7', '$2y$10$HvkMJs8/qjovz2axkZb38.Ewm8Og.sGQwsYBJhYxIGM/V6yIYAMhC', 'rosita.melanito@gmail.com', 1, ''),
+(28, 'mariano7', '$2y$10$LOA1b4eMHorKSkpLTuJKjODTNQFiDhQtiiCW8d9DeWbM85OUytqMi', 'rosita.melanito36@gmail.com', 1, ''),
+(29, 'alanzelada12', '$2y$10$iYSjEsafX6RDwNlRhI1cjOKanF4s0lkXPq/EHZ5sJqEbBaShMi9qS', 'alanzelada9@gmail.com', 1, ''),
+(30, 'daasdadaddsad', '$2y$10$vzjB1k71WA9Rpx7OTbE3COesWSW.Kstkuw7nkPfxwxOtwkEMsyVQG', 'adasdasdd@dadasdasd.com', 1, ''),
+(38, 'siiiiiiiiiiiii', '$2y$10$nh12PAHqXGP2nC6Bjc1rKOWU2lEz83DPa9CTVWhkDZ.ZCaotY5Xj2', 'cmftresd@gmail.com', 1, '');
 
 --
 -- Índices para tablas volcadas
@@ -106,7 +107,14 @@ INSERT INTO `usuarios` (`ID_u`, `Nbr_u`, `Pass_u`, `email_u`, `token_u`) VALUES
 -- Indices de la tabla `articulos`
 --
 ALTER TABLE `articulos`
-  ADD PRIMARY KEY (`id_articulo`);
+  ADD PRIMARY KEY (`id_articulo`),
+  ADD KEY `fk_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD PRIMARY KEY (`id_pedido`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -122,13 +130,29 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `articulos`
 --
 ALTER TABLE `articulos`
-  MODIFY `id_articulo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_articulo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT de la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  MODIFY `id_pedido` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `ID_u` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `ID_u` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `articulos`
+--
+ALTER TABLE `articulos`
+  ADD CONSTRAINT `fk_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`ID_u`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
