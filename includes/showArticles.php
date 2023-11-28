ace0b537a5213a3083b42b2444b66ea38a953528
MarianoJSB <mariano.n.aguiar@gmail.com>
2023-11-28 14:31:13 -0300

    Show username article

--- includes/showArticles.php
+++ includes/showArticles.php
@@ -222,13 +222,7 @@
                 $format = number_format($registro['precio'], 2, ',', '.');
                 echo '
                 <div class="userName">';
-                if(isset($registro['Nbr_u'])){
-                    echo '<p class="user">'.$registro_u['Nbr_u'].'</p>';
-                }else{
-                    echo '<p class="user"> - </p>';
-                }
-                    
-                echo '
+                    echo '<p class="user">'.$registro_u['Nbr_u'].'</p>
                 </div>
                 <div class="container">
                     <div class="imgContainer">
@@ -294,76 +288,6 @@
         }
         }
 
-        // Editar publicacion
-        function editArticle($id_a){
-            include("conexion.php");
-
-            $sql = "SELECT * FROM articulos WHERE id_articulo = '$id_a'";
-            $query = mysqli_query($conexion, $sql);
-            $registro = mysqli_fetch_assoc($query);
-
-            echo '<form action="" method="POST">
-            <input type="text" name="articulo" placeholder='.$registro['nombre'].' min="5" max="30" pattern=".{5,30}" required class="name">
-            <textarea class="description" cols="30" rows="10" name="descripcion" max="100" pattern=".{0,100}" placeholder='.$registro['descripcion'].'></textarea>
-            <input type="number" name="precio" placeholder='.$registro['precio'].' min="1" max="1000000000" required class="price">
-            <input type="number" name="stock" placeholder='.$registro['stock'].' min="1" max="100000" required class="stock">
-            <select name="color" class="selects">
-                <option disabled selected value="">'.$registro['color'].'</option>
-                <option value="azul">Azul</option>
-                <option value="rojo">Rojo</option>
-                <option value="verde">Verde</option>
-                <option value="amarillo">Amarillo</option>
-                <option value="violeta">Violeta</option>
-                <option value="gris">Gris</option>
-                <option value="negro">Negro</option>
-                <option value="blanco">Blanco</option>
-                <option value="naranja">Naranja</option>
-                <option value="varios">Varios</option>
-                <option value="ninguno">Ninguno</option>
-            </select>
-            <select name="categoria" class="selects">
-                <option disabled selected value="">'.$registro['categoria'].'</option>
-                <option value="Auto">Auto</option>
-                <option value="Moto">Moto</option>
-                <option value="Formula">Formula</option>
-                <option value="varias">Varias</option>
-                <option value="ninguna">Ninguna</option>
-            </select>
-            <select name="estado" class="selects">
-                <option disabled selected value="">'.$registro['estado'].'</option>
-                <option value="nuevo">Nuevo</option>
-                <option value="usado">usado</option>
-                <option value="ninguno">Ninguno</option>
-            </select>
-            <input type="submit" value="Actualizar datos" name="actualizar" style="cursor:pointer" class="sendData">
-            </form>
-            <a href="perfil.php?cancel" class="cancel">
-            <img src="src/images/mark.svg">
-            </a>';
-            if(isset($_GET['cancel'])){
-                header('Location: perfil.php');
-            exit();
-            }else{
-                echo '<a href="perfil.php?id_editar_articulo='.$registro['id_articulo'].'">
-                        <img src="src/images/pencil.svg">
-                    </a>';
-            }
-
-            if(isset($_POST['actualizar'])){
-                // Datos a actualizar
-                $nombre = $_POST['articulo'];
-                $descripcion = $_POST['descripcion'];
-                $precio = $_POST['precio'];
-                $stock = $_POST['stock'];
-                $color = $_POST['color'];
-                $categoria = $_POST['categoria'];
-                $estado = $_POST['estado'];
-
-                $sqlUpdate = "UPDATE articulos SET nombre = '$nombre', descripcion = '$descripcion', precio = '$precio', stock = '$stock', color = '$color', categoria = '$categoria', estado = '$estado' WHERE id_articulo = '$id_a'";
-                $update = mysqli_query($conexion, $sqlUpdate);
-
-            }
-        }
 ?>
 
 </body>
