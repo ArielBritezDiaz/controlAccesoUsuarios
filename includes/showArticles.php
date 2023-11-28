008c15d965ea77521c4c69fc27c597b4b145ca8c
MarianoJSB <mariano.n.aguiar@gmail.com>
2023-11-27 21:55:30 -0300

    Don't show articles with 0 stock

--- includes/showArticles.php
+++ includes/showArticles.php
@@ -23,6 +23,7 @@
         $query = mysqli_query($conexion, $sql);
         echo '<script>window.location.hash = "#stay";</script>';
         while($registro=mysqli_fetch_assoc($query)){
+            if($registro['stock'] > 0){
             $format = number_format($registro['precio'], 2, ',', '.');
             echo'<div class="card">
                         <a href="publishing.php?id_art='.$registro['id_articulo'].'">
@@ -35,6 +36,7 @@
                 </div>';
         }
     }
+}
 
     //Mostrar articulos ordenados de forma descendente
     function orderDesc($buscar = ""){
@@ -43,6 +45,7 @@
         $query = mysqli_query($conexion, $sql);
         echo '<script>window.location.hash = "#stay";</script>';
         while($registro=mysqli_fetch_assoc($query)){
+            if($registro['stock'] > 0){
             $format = number_format($registro['precio'], 2, ',', '.');
             echo'<div class="card">
                         <a href="publishing.php?id_art='.$registro['id_articulo'].'">
@@ -55,13 +58,14 @@
                 </div>';
         }
     }
-
+}
     //Mostrar todos los articulos
     function showArticles(){
         include ("conexion.php");
         $sql = "SELECT * FROM articulos";
         $query = mysqli_query($conexion, $sql);
         while($article = mysqli_fetch_assoc($query)){
+            if($article['stock'] > 0){
             // Precio formateado //
             $format = number_format($article['precio'], 2, ',', '.');
             echo'<div class="card">
@@ -75,6 +79,7 @@
             </div>';
         }
     }
+}
 
     //Mostrar articulos por categoria
     function showCategorie($cat){
@@ -83,6 +88,7 @@
         $query = mysqli_query($conexion, $sql);
         echo '<script>window.location.hash = "#stay";</script>';
         while($article = mysqli_fetch_assoc($query)){
+            if($article['stock'] > 0){
             // Precio formateado //
             $format = number_format($article['precio'], 2, ',', '.');
             echo'<div class="card">
@@ -99,6 +105,7 @@
             echo '<p class="empty">No hay stock de articulos con la categoria <span class="bold">'.$cat.'</span></p>';
         }
     }
+}
 
     //Ordenar articulos por categoria de forma ascendente
     function orderAscCategorie($cat){
@@ -107,6 +114,7 @@
         $query = mysqli_query($conexion, $sql);
         echo '<script>window.location.hash = "#stay";</script>';
         while($article = mysqli_fetch_assoc($query)){
+            if($article['stock'] > 0){
             // Precio formateado //
             $format = number_format($article['precio'], 2, ',', '.');
             echo'<div class="card">
@@ -123,6 +131,7 @@
             echo '<p class="empty">No hay stock de articulos con la categoria <span class="bold">'.$cat.'</span></p>';
         }
     }
+}
 
     //Ordenar articulos por categoria de forma descendente
     function orderDescCategorie($cat){
@@ -131,6 +140,7 @@
         $query = mysqli_query($conexion, $sql);
         echo '<script>window.location.hash = "#stay";</script>';
         while($article = mysqli_fetch_assoc($query)){
+            if($article['stock'] > 0){
             // Precio formateado //
             $format = number_format($article['precio'], 2, ',', '.');
             echo'<div class="card">
@@ -147,7 +157,7 @@
             echo '<p class="empty">No hay stock de articulos con la categoria <span  class="bold">'.$cat.'</span></p>';
         }
     }
-
+}
     // Mostrar las publicaciones de los usuarios
     function showPublishing($id_user){
         include("conexion.php");
@@ -285,74 +295,75 @@
         }
 
         // Editar publicacion
-        // function editArticle($id_a){
-        //     include("conexion.php");
+        function editArticle($id_a){
+            include("conexion.php");
 
-        //     $sql = "SELECT * FROM articulos WHERE id_articulo = '$id_a'";
-        //     $query = mysqli_query($conexion, $sql);
-        //     $registro = mysqli_fetch_assoc($query);
+            $sql = "SELECT * FROM articulos WHERE id_articulo = '$id_a'";
+            $query = mysqli_query($conexion, $sql);
+            $registro = mysqli_fetch_assoc($query);
 
-        //     echo '<form action="" method="POST">
-        //     <input type="text" name="articulo" placeholder='.$registro['nombre'].' min="5" max="30" pattern=".{5,30}" required class="name">
-        //     <textarea class="description" cols="30" rows="10" name="descripcion" max="100" pattern=".{0,100}" placeholder='.$registro['descripcion'].'></textarea>
-        //     <input type="number" name="precio" placeholder='.$registro['precio'].' min="1" max="1000000000" required class="price">
-        //     <input type="number" name="stock" placeholder='.$registro['stock'].' min="1" max="100000" required class="stock">
-        //     <select name="color" class="selects">
-        //         <option disabled selected value="">'.$registro['color'].'</option>
-        //         <option value="azul">Azul</option>
-        //         <option value="rojo">Rojo</option>
-        //         <option value="verde">Verde</option>
-        //         <option value="amarillo">Amarillo</option>
-        //         <option value="violeta">Violeta</option>
-        //         <option value="gris">Gris</option>
-        //         <option value="negro">Negro</option>
-        //         <option value="blanco">Blanco</option>
-        //         <option value="naranja">Naranja</option>
-        //         <option value="varios">Varios</option>
-        //         <option value="ninguno">Ninguno</option>
-        //     </select>
-        //     <select name="categoria" class="selects">
-        //         <option disabled selected value="">'.$registro['categoria'].'</option>
-        //         <option value="Auto">Auto</option>
-        //         <option value="Moto">Moto</option>
-        //         <option value="Formula">Formula</option>
-        //         <option value="varias">Varias</option>
-        //         <option value="ninguna">Ninguna</option>
-        //     </select>
-        //     <select name="estado" class="selects">
-        //         <option disabled selected value="">'.$registro['estado'].'</option>
-        //         <option value="nuevo">Nuevo</option>
-        //         <option value="usado">usado</option>
-        //         <option value="ninguno">Ninguno</option>
-        //     </select>
-        //     <input type="submit" value="Actualizar datos" name="actualizar" style="cursor:pointer" class="sendData">
-        //     </form>
-        //     <a href="perfil.php?cancel" class="cancel">
-        //     <img src="src/images/mark.svg">
-        //     </a>';
-        //     if(isset($_GET['cancel'])){
-        //         header('Location: perfil.php');
-        //     exit();
-        //     }else{
-        //         echo '<a href="perfil.php?id_editar_articulo='.$registro['id_articulo'].'">
-        //                 <img src="src/images/pencil.svg">
-        //             </a>';
-        //     }
+            echo '<form action="" method="POST">
+            <input type="text" name="articulo" placeholder='.$registro['nombre'].' min="5" max="30" pattern=".{5,30}" required class="name">
+            <textarea class="description" cols="30" rows="10" name="descripcion" max="100" pattern=".{0,100}" placeholder='.$registro['descripcion'].'></textarea>
+            <input type="number" name="precio" placeholder='.$registro['precio'].' min="1" max="1000000000" required class="price">
+            <input type="number" name="stock" placeholder='.$registro['stock'].' min="1" max="100000" required class="stock">
+            <select name="color" class="selects">
+                <option disabled selected value="">'.$registro['color'].'</option>
+                <option value="azul">Azul</option>
+                <option value="rojo">Rojo</option>
+                <option value="verde">Verde</option>
+                <option value="amarillo">Amarillo</option>
+                <option value="violeta">Violeta</option>
+                <option value="gris">Gris</option>
+                <option value="negro">Negro</option>
+                <option value="blanco">Blanco</option>
+                <option value="naranja">Naranja</option>
+                <option value="varios">Varios</option>
+                <option value="ninguno">Ninguno</option>
+            </select>
+            <select name="categoria" class="selects">
+                <option disabled selected value="">'.$registro['categoria'].'</option>
+                <option value="Auto">Auto</option>
+                <option value="Moto">Moto</option>
+                <option value="Formula">Formula</option>
+                <option value="varias">Varias</option>
+                <option value="ninguna">Ninguna</option>
+            </select>
+            <select name="estado" class="selects">
+                <option disabled selected value="">'.$registro['estado'].'</option>
+                <option value="nuevo">Nuevo</option>
+                <option value="usado">usado</option>
+                <option value="ninguno">Ninguno</option>
+            </select>
+            <input type="submit" value="Actualizar datos" name="actualizar" style="cursor:pointer" class="sendData">
+            </form>
+            <a href="perfil.php?cancel" class="cancel">
+            <img src="src/images/mark.svg">
+            </a>';
+            if(isset($_GET['cancel'])){
+                header('Location: perfil.php');
+            exit();
+            }else{
+                echo '<a href="perfil.php?id_editar_articulo='.$registro['id_articulo'].'">
+                        <img src="src/images/pencil.svg">
+                    </a>';
+            }
 
-        //     if(isset($_POST['actualizar'])){
-        //         // Datos a actualizar
-        //         $nombre = $_POST['articulo'];
-        //         $descripcion = $_POST['descripcion'];
-        //         $precio = $_POST['precio'];
-        //         $stock = $_POST['stock'];
-        //         $color = $_POST['color'];
-        //         $categoria = $_POST['categoria'];
-        //         $estado = $_POST['estado'];
+            if(isset($_POST['actualizar'])){
+                // Datos a actualizar
+                $nombre = $_POST['articulo'];
+                $descripcion = $_POST['descripcion'];
+                $precio = $_POST['precio'];
+                $stock = $_POST['stock'];
+                $color = $_POST['color'];
+                $categoria = $_POST['categoria'];
+                $estado = $_POST['estado'];
 
-        //         $sqlUpdate = "UPDATE articulos SET nombre = '$nombre', descripcion = '$descripcion', precio = '$precio', stock = '$stock', color = '$color', categoria = '$categoria', estado = '$estado' WHERE id_articulo = '$id_a'";
-        //         $update = mysqli_query($conexion, $sqlUpdate);
+                $sqlUpdate = "UPDATE articulos SET nombre = '$nombre', descripcion = '$descripcion', precio = '$precio', stock = '$stock', color = '$color', categoria = '$categoria', estado = '$estado' WHERE id_articulo = '$id_a'";
+                $update = mysqli_query($conexion, $sqlUpdate);
 
-        //     }
+            }
+        }
 ?>
 
 </body>
